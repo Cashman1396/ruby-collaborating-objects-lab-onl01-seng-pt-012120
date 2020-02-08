799ccf5ff44452e2e133e5936cc0cf2385499aae
Chris Ashman <chrisashman96@gmail.com>
2020-02-08 00:17:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,6 +11,10 @@ class Song
     @@all << self 
   end 
   
+  def self.all
+    @@all
+  end 
+  
   def self.new_by_filename(filename)
     artist, song = filename.split(" - ")
     new_song = self.new(song)
